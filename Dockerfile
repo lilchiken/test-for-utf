d638c0bdefb7c8ea10dbb649ca435151813e25e5
localmachine <lilchiken>
2023-06-11 17:41:16 +0300

    fixes docker

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 COPY requirements.txt /app
 RUN pip3 install -r requirements.txt --no-cache-dir
 COPY . /app 
-ENTRYPOINT ["/bin/bash", "/app/django-entrypoint.sh"] 
-CMD ["setenv"]
-CMD ["migrate"]
-CMD ["start"]
+RUN chmod u+x /app/django-entrypoint.sh
+RUN /bin/bash /app/django-entrypoint.sh migrate
+ENTRYPOINT ["python3"] 
+CMD ["manage.py", "runserver", "0.0.0.0:80"]
--- django-entrypoint.sh
+++ django-entrypoint.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
 case $1 in
-  setenv)
+  migrate)
     export DJANGO_SUPERUSER_EMAIL=admin@admin.com
     export DJANGO_SUPERUSER_USERNAME=admin
     export DJANGO_SUPERUSER_PASSWORD=admin
-    ;;
-  migrate)
     python3 manage.py migrate
     python3 manage.py createsuperuser --username=admin --email=admin@admin.com --noinput
-    ;;
-  start)
-    python3 manage.py runserver 0.0.0.0:80
     ;;
 esac
